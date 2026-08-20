aaa344ad66997144ab836bab2bf9458c4a9cc6a8
Rene <rrr333nnn333@gmail.com>
2026-08-19 21:09:40 -0400

    fix: webui: annoying line break

--- module/webroot/script.js
+++ module/webroot/script.js
@@ -449,13 +449,19 @@ exec(`cat ${PERSISTENT_DIR}/config.sh`).then((result) => {
 		}
 
 		if (file) {
-			exec(`
+			if (content === '') {
+				exec(`printf '' > ${PERSISTENT_DIR}/${file}`).then((result) => {
+					toast(result.errno === 0 ? 'Success' : result.stderr)
+				})
+			} else {
+				exec(`
 cat <<'UNIQUE_EOF' > ${PERSISTENT_DIR}/${file}
 ${content}
 UNIQUE_EOF
-		`).then((result) => {
-				toast(result.errno === 0 ? 'Success' : result.stderr)
-			})
+				`).then((result) => {
+					toast(result.errno === 0 ? 'Success' : result.stderr)
+				})
+			}
 		}
 	}
 })()
