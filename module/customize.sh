c5e2501362b7d2c0dadc29437ff8a86eee6e2e48
Tomkun-desu <southkaz@keemail.me>
2026-09-04 09:43:11 +0330

    improve: enable WebUI without reboot

--- module/customize.sh
+++ module/customize.sh
@@ -134,3 +134,21 @@ done
 if [[ -e "/data/adb/modules/playintegrityfix" ]] && grep -q "Integrity-Box" "/data/adb/modules/playintegrityfix/module.prop"; then
         touch "/data/adb/modules/playintegrityfix/remove"
 fi
+
+# Enable WebUI without reboot
+MODDIR="/data/adb/modules/brene"
+MODULES_PATH="/data/adb/modules"
+
+rm -rf "${MODDIR}"
+mv "${MODPATH}" "${MODULES_PATH}"
+
+mkdir -p "${MODPATH}"
+cp "${MODDIR}/module.prop" "${MODPATH}"
+
+(
+        sleep 3
+        rm -rf "${MODPATH}"
+        rm "${MODDIR}/update"
+) & # fork in background
+
+echo '[✅] WebUI is ready!'
